2bd8366e56ed0306d50ccbcf749ab1ea3851c0ce
YuvaSri110102 <yuvasri9392@gmail.com>
2025-06-19 23:06:32 +0530

    app.py changes

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ st.write(f'Churn Probability: {prediction_proba:.2f}')
 if prediction_proba > 0.5:
     st.write('The customer is likely to churn.')
 else:
+    
     st.write('The customer is not likely to churn.')
